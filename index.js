f4d9786cd5e28798d3159878a019ebf02614de57
Harsh641-1 <187022749+Harsh641-1@users.noreply.github.com>
2025-10-31 08:45:33 +0530

    Create operate function

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ function divide(num1, num2){
     return num1 / num2
 }
 
+const num1 = Number(prompt('Enter first number:'))
+const num2 = Number(prompt('Enter second number:'))
+const operator = prompt('Enter operation')
+
+function operate(num1, num2, operator){
+    if (operator === '+'){
+        result = add(num1, num2)
+    } else if (operator === '-'){
+        result = subtract(num1, num2)
+    }else if (operator === '*') {
+        result = multiply(num1, num2)
+    }else if (operator === '/'){
+        result = divide(num1, num2)
+    } else {
+        result = 'Enter correct operator'
+    }
+    return result
+}
+
+console.log(operate(num1, num2, operator))
